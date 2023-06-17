e0e6eab64b7a96c70cafbd46ef127c28a1233d76
sumanmunde123 <93462136+sumanmunde123@users.noreply.github.com>
2023-06-17 15:15:25 +0530

    third commit

--- src/Component/CompanyUI.jsx
+++ src/Component/CompanyUI.jsx
@@ -14,9 +14,10 @@ const CompanyUI = () => {
 const [user, setUser] = useState([])
   
     const fetchData = async() => {
-        const res = await fetch("http://localhost:8081/Data");
-        setUser(await res.json());
-        // console.log("data:", data)
+        const res = await fetch("http://localhost:8080/Data");
+        const data = await res.json();
+        setUser(data)
+        console.log("data:", data)
         
       }
       useEffect(()=>{
@@ -25,12 +26,12 @@ const [user, setUser] = useState([])
       },[])
 
 
-
+console.log("user", user[0])
   return (
     <>
        <div className='navbar'>
          <img className='logoi' src='./images/logo1.png'></img>
-         <Button  variant="outlined" startIcon={<SearchIcon />}>
+         <Button sx={{ color: 'black',}}  variant="outlined" startIcon={<SearchIcon />}>
   Search
 </Button>
          <div className="bell">
@@ -54,7 +55,180 @@ const [user, setUser] = useState([])
        </div>
 
        <div className="sidebar">
-       <Button className='color' variant="text">Create a Job</Button>
+       <Button  sx={{ color: 'black',}} variant="text">Create a Job</Button>
+       </div>
+
+
+
+
+
+       {/* fetch data */}
+
+       <div className="fetchdata">
+        <h3>Create a Job</h3>
+        <p>Select Zendex Documents</p>
+      <div className="box">
+      <Autocomplete
+      disablePortal
+      id="combo-box-demo"
+      size="small"
+      sx={{ width: 250 } }
+      renderInput={(params) => <TextField {...params} label="" />}
+    />
+      </div>
+
+
+      <div className="inside">
+    <div className="fetch_i">
+      <h3>Company</h3>
+
+      {
+        user.length > 0 && user[0].Company.map((e) => {
+          return(
+            <>
+         <div className="boxi">
+         <div className="lablei">
+            <label>Company Name</label>
+            <div>{e.companyName}</div>
+          </div>
+          <div className="lablei">
+            <label>Company Type</label>
+            <div>{e.companyType}</div>
+          </div>
+         </div>
+         <div className="lablei">
+            <label>Address</label>
+            <div>{e.Address}</div>
+          </div>
+          <div className="lablei">
+            <label>Phone</label>
+            <div>{e.Phone}</div>
+          </div>
+            </>
+          )
+        })
+      }
+    </div>
+    <div className="fetch_i">
+    <h3>User</h3>
+    {
+        user.length > 0 && user[0].User.map((e) => {
+          return(
+            <>
+         <div className="boxi">
+         <div className="lablei">
+            <label>First Name</label>
+            <div>{e.firstName}</div>
+          </div>
+          <div className="lablei">
+            <label>Last Name</label>
+            <div>{e.lastName}</div>
+          </div>
+         </div>
+         <div className="lablei">
+            <label>Address</label>
+            <div>{e.Address}</div>
+          </div>
+          <div className="lablei">
+            <label>Phone</label>
+            <div>{e.Phone}</div>
+          </div>
+            </>
+          )
+        })
+      }
+    </div>
+    <div className="fetch_i">
+      <h3>Server</h3>
+    {
+        user.length > 0 && user[0].Server.map((e) => {
+          return(
+            <>
+              <div className="lablei">
+            <label>States</label>
+            <div>{e.states}</div>
+          </div>
+          <div className="lablei">
+            <label>Countries</label>
+            <div>{e.countries}</div>
+          </div>
+          <div className="lablei">
+            <label>zipCodes</label>
+            <div>{e.zipcodes}</div>
+          </div>
+         <div className="boxi">
+         <div className="lablei">
+            <label>Standard Rates</label>
+            <div>{e.standardRates}</div>
+          </div>
+          <div className="lablei">
+            <label>Two Day Rates</label>
+            <div>{e.TwoDayRates}</div>
+          </div>
+         </div>
+         <div className="boxi">
+         <div className="lablei">
+            <label>Onde Day Rates</label>
+            <div>{e.OneDayRates}</div>
+          </div>
+          <div className="lablei">
+            <label>same Day Rates</label>
+            <div>{e.SameDayRate}</div>
+          </div>
+         </div>
+         <div className="lablei">
+            <label>Number Of Attempts</label>
+            <div>{e.NumberOfAttempts}</div>
+          </div>
+        
+            </>
+          )
+        })
+      }
+    </div>
+    <div className="fetch_i">
+      <h3>Documents</h3>
+    {
+        user.length > 0 && user[0].Documents.map((e) => {
+          return(
+            <>
+         <div className="boxi">
+         <div className="lablei">
+            {/* <label>Onde Day Rates</label> */}
+            <div>Document 1</div>
+          </div>
+          <div className="lablei">
+           
+            <label>{e.Document1}</label>
+          </div>
+         </div>
+         <div className="boxi">
+         <div className="lablei">
+            {/* <label>Onde Day Rates</label> */}
+            <div>Document 2</div>
+          </div>
+          <div className="lablei">
+           
+            <label>{e.Document2}</label>
+          </div>
+         </div>
+         <div className="boxi">
+         <div className="lablei">
+            {/* <label>Onde Day Rates</label> */}
+            <div>Document 3</div>
+          </div>
+          <div className="lablei">
+           
+            <label>{e.Document3}</label>
+          </div>
+         </div>
+            </>
+          )
+        })
+      }
+    </div>
+    </div>
+
        </div>
     </>
   )
